0fb300497296f163511b66e76418c6576aec0285
Adam Kolar <akolar@adscanner.tv>
2024-09-10 22:08:37 +0200

    w++

--- UnitStatsCompare/stats_compare.py
+++ UnitStatsCompare/stats_compare.py
@@ -209,7 +209,7 @@ def open_simulation_config_command():
 def main():
 	root = data.root = Tk()
 	root.title(f'BK2 Unit Stats Compare {PROGRAM_VERSION}')
-	root.geometry("1200x1050")
+	root.geometry("1220x1080")
 	root.minsize(800, 600)
 	#root.iconbitmap("icon.ico")
 
